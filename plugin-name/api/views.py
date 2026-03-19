09471c82678e6ffef47edc913299f5b8b1de8bd9
Francisc0Lopes <f.alexandre.lopes.03@gmail.com>
2026-03-19 10:55:01 +0000

    Views.py

--- plugin-name/api/views.py
+++ plugin-name/api/views.py
@@ -1,10 +1,10 @@
-from dcim.models import Interface, Cable
+from dcim.models import Interface
 from ipam.models import VLAN
 
 def get_vlan(ID_vlan):
     
     try:
-        vlan = VLAN.objects.get(id= ID_vlan)
+        vlan = VLAN.objects.get(id=ID_vlan)
     except VLAN.DoesNotExist:
         return {"Erro": "VLAN não existe"}
     
@@ -16,6 +16,41 @@ def get_vlan(ID_vlan):
     for interface in interfaces:
         Equip = interface.device
         
-        if not any
-    
-    
+        # Adicionar Nó
+        if not any(n['id'] == Equip.id for n in nos):
+            nos.append({
+                "id": Equip.id,
+                "name": Equip.name, 
+                "role": Equip.device_role.name 
+            })
+        
+        # Processar Cabos e Ligações
+        if interface.cable and interface.cable.id not in cabos_processados:
+            cabos = interface.cable
+            cabos_processados.add(cabos.id)
+            
+            path_destino = cabos.get_path_endpoints() #Descobrir o path destino
+            remote_interface = None
+            
+            for endpoint in path_destino:
+                if endpoint != interface:
+                    remote_interface = endpoint
+                    break
+            
+            if isinstance(remote_interface, Interface):#Caso o outro lado seja valido
+                
+                is_remote_access = (remote_interface.untagged_vlan == vlan)
+                is_remote_trunk = (vlan in remote_interface.tagged_vlans.all())
+                
+                if is_remote_access or is_remote_trunk:
+                    ligacoes.append({
+                        "source" : Equip.id, 
+                        "target" : remote_interface.device.id, 
+                        "source_port" : interface.name, 
+                        "source_mode": "Trunk" if vlan in interface.tagged_vlans.all() else "Access",
+                        "target_port": remote_interface.name,
+                        "target_mode": "Trunk" if is_remote_trunk else "Access", 
+                        "stp_state": "Forwarding" 
+                    })       
+                    
+    return {"vlan": vlan.vid, "nos": nos, "ligacoes": ligacoes}
